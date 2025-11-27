6de513e7d295ebda120982ffa13cad2087e541b0
0xJakuzya <kuzmin_dima2005@mail.ru>
2025-11-27 16:54:14 +0400

    commit

---
+++ project/app/models/__init__.py
@@ -0,0 +1,19 @@
+from .vehicle_type import VehicleType
+from .client import Client
+from .vehicle import Vehicle
+from .parking_space import ParkingSpace
+from .tariff import Tariff
+from .parking_session import ParkingSession
+from .payment_method import PaymentMethod
+from .payment import Payment
+
+__all__ = [
+    "VehicleType",
+    "Client",
+    "Vehicle",
+    "ParkingSpace",
+    "Tariff",
+    "ParkingSession",
+    "PaymentMethod",
+    "Payment"
+]
